6a7d900c88aa364902cf38c4216f68ee95ce9e2f
jessicacoutot <jeh_trajano@hotmail.com>
2024-11-27 15:15:15 -0300

    conclusao

---
+++ index.js
@@ -0,0 +1,32 @@
+let nameHero = "Caio"
+let xp = 1.000
+let nivel = ""
+
+switch (true){
+    case xp <= 1.000:
+        nivel = "Ferro"
+        break
+    case xp > 1.001 && xp <= 2.000:
+        nivel = "Bronze"
+        break
+    case xp > 2.001 && xp <= 5.000:
+        nivel = "Prata"
+        break
+    case xp > 5.001 && xp <= 7.000:
+        nivel = "Ouro"
+        break
+    case xp > 7.001 && xp <= 8.000:
+        nivel = "Platina"
+        break
+    case xp > 8.001 && xp <= 9.000:
+        nivel= "Ascendente"
+        break
+    case xp > 9.001 && xp <= 10.000:
+        nivel = "Imortal"
+        break
+    case xp >= 10.001:
+        nivel = "Radiante"
+        break
+}
+
+console.log("O Herói de nome " + nameHero + " está no nível " + nivel)
